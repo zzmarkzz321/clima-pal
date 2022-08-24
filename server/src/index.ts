9e6ba0a02a67ca6e65713a13f435a80d9a9430ec
zzmarkzz321 <markrenzosantiago@gmail.com>
2022-08-23 21:12:07 -0700

    update start command

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,6 +15,24 @@ app.get("/", (_, res) => {
 	res.send("hello world");
 });
 
+// Records
+// Challenges (available challenges)
+// Users (Maps to a monday.com user)
+// Accomplishments (Tied to users to record which challenge they completed)
+
+app.put("challenges/:challengeId", () => {
+	// Once 9am hits, update current challenge active field to false and selecting a new challenge by setting active to true
+	// This endpoint does the mutation. Timing can be done via aws
+});
+
+app.post("/accomplishments/challenge/:challengeId/user/:userId", () => {
+	// Creating an accomplishment record for a user
+});
+
+app.post("users", () => {
+	// Creating a user record for new players
+});
+
 app.listen(3001, () => {
 	console.log("listening on port 3001");
 });
